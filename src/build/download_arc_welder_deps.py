f95586960bc1b71e2ee9f711b22c3746e422d203
arc-push <arc-push@chromium.org>
2015-10-13 09:25:08 -0700

    Updated to arc-runtime-47.5021.509.0

--- src/build/download_arc_welder_deps.py
+++ src/build/download_arc_welder_deps.py
@@ -11,7 +11,8 @@ def check_and_perform_updates(cache_base_path, cache_history_size):
       'src/build/DEPS.naclports-python',
       'out/naclports-python',
       cache_base_path=cache_base_path,
-      cache_history_size=cache_history_size
+      cache_history_size=cache_history_size,
+      download_method=download_package_util.gsutil_download_url()
   ).check_and_perform_update()
 
   download_package_util.BasicCachedPackage(
